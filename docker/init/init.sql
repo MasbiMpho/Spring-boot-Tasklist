4b6063e6dc519f94cd8391f60d2a31012154a034
MasbiMpho <mpholin15@gmail.com>
2025-09-30 15:11:36 +0200

    Made some minor improvements to database schema

--- docker/init/init.sql
+++ docker/init/init.sql
@@ -7,13 +7,13 @@ CREATE TABLE IF NOT EXISTS task (
     due_date date
 );
 
-INSERT INTO task (name, status, due_date) VALUES 
-('Create a tasklist', 'Uncompleted', '2025-09-24'),
-('Task 3',	'completed',	'2025-09-27'),
-('Task 4', 'uncompleted', '2025-09-29'),
-('Task 2', 'uncompleted', '2025-09-26'),
-('Task 5', 'uncompleted', '2025-09-30');
-
+INSERT INTO task (name, status, due_date) VALUES
+('Create database', 'completed', '2025-09-24'), 
+('Create a spring boot tasklist', 'completed', '2025-09-25'),
+('Create REST API endpoints', 'completed', '2025-09-26'),
+('Test API endpoints using postman',	'uncompleted',	'2025-09-27'),
+('Implement Swagger', 'uncompleted', '2025-09-28'),
+('Make data persistent', 'uncompleted', '2025-09-30');
 
 
 SELECT setval(pg_get_serial_sequence('public.task', 'id'), (SELECT MAX(id) FROM public.task));
